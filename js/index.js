7a428933ec91e212d0fdd509838223092817b937
SekoVper <damehlivingstone1@gmail.com>
2022-10-31 14:50:01 +0100

    Modify script file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("Working")
+import { displayBooks, addFormEventListener } from '../module/awesomebooks.js';
+import showComponent from '../module/component.js';
+import { DateTime } from '../module/luxon.js';
+
+const formElem = document.querySelector('form');
+const containerElem = document.getElementById('book-detail');
+const navLinks = document.querySelectorAll('.menu-items li');
+const currentDateEl = document.getElementById('current-date');
+
+setInterval(() => {
+  currentDateEl.textContent = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+}, 1000);
+
+showComponent(navLinks);
+displayBooks(containerElem);
+addFormEventListener(formElem, containerElem);
